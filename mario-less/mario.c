90ba1812f00e8587baaf041c86176a7b6cdd4572
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-02-27 11:14:29 +0000

    Tue, Feb 27, 2024, 6:14 PM +07:00

---
+++ mario-less/mario.c
@@ -0,0 +1,26 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void pyramid(int n);
+int main(void)
+{
+
+int num;
+    do
+    {
+        num = get_int("Height: ");
+    } while(num <= 0);
+    pyramid(num);
+}
+
+void pyramid(int n)
+{
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<i; j++)
+        {
+            printf(" ");
+        }
+        printf("#\n");
+    }
+}
