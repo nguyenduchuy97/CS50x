529ed66b77d85468dd92955306b582c7db3f7b3c
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-02-27 14:18:24 +0000

    Tue, Feb 27, 2024, 9:18 PM +07:00

---
+++ mario-more/mario.c
@@ -0,0 +1,38 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void print_row(int spaces, int bricks);
+int main(void)
+{
+
+    int num;
+    do
+    {
+        num = get_int("Height: ");
+    }
+    while (num < 1);
+    for (int i = 0; i < num; i++)
+    {
+        print_row(num - i, i + 1);
+    }
+}
+
+void print_row(int spaces, int bricks)
+{
+    for (int i = 0; i < spaces; i++)
+    {
+        if (i == 0)
+        {
+            printf("");
+        }
+        else
+        {
+            printf(" ");
+        }
+    }
+    for (int j = 0; j < bricks; j++)
+    {
+        printf("#");
+    }
+    printf("\n");
+}
