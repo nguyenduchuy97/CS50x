555bf519ed72bb6be941948c37263234b1343022
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-02-27 11:46:06 +0000

    Tue, Feb 27, 2024, 6:46 PM +07:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -18,9 +18,9 @@ void pyramid(int n)
 {
     for(int i=0; i<n; i++)
     {
-        for(int j=0; j<=n; j++)
+        for(int j=n-1; j=0; j++)
         {
-            printf("#");
+            printf(" ");
         }
         printf("\n");
     }
