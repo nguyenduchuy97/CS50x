1b4d87cbff3f7a86166570311af8a407373d04f5
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-02-27 13:39:35 +0000

    Tue, Feb 27, 2024, 8:39 PM +07:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -5,21 +5,21 @@ void print_row(int bricks);
 int main(void)
 {
 
-int num;
+    int num;
     do
     {
         num = get_int("Height: ");
     }
-    while(num < 1);
-    for(int i=0;i<num;i++)
+    while (num < 1);
+    for (int i = 0; i < num; i++)
     {
-        print_row(num-i);
+        print_row(num - i);
     }
 }
 
 void print_row(int bricks)
 {
-    for(int i=0;i<bricks;i++)
+    for (int i = 0; i < bricks; i++)
     {
         printf("#");
     }
