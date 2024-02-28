f8b10b79bc518e45da49f895c7bd2b792947d280
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-02-28 03:24:39 +0000

    Wed, Feb 28, 2024, 10:24 AM +07:00

--- cash/cash.c
+++ cash/cash.c
@@ -2,6 +2,10 @@
 #include <stdio.h>
 
 int calculate_quarters(int cents);
+int calculate_dimes(int cents);
+int calculate_nickels(int cents);
+int calculate_pennies(int cents);
+int calculate_sum(int quarter, int dimes, int nickels, int pennies);
 
 int main(void)
 {
@@ -12,7 +16,9 @@ int main(void)
         cents = get_int("Change owed: ");
     }
     while (cents < 0);
+    int calculate_sum(int quarters, int dimes, int nickels, int pennies);
 }
+
     // Calculate how many quarters you should give customer
 int calculate_quarters(int cents)
 {
@@ -20,22 +26,59 @@ int calculate_quarters(int cents)
 
     while (cents > 24)
     {
+    // Subtract the value of those quarters from cents
         cents -= 25;
         quarters ++;
     }
     return quarters;
 }
-    // Subtract the value of those quarters from cents
 
     // Calculate how many dimes you should give customer
-    // Subtract the value of those dimes from remaining cents
+int calculate_dimes(int cents)
+{
+    int dimes = 0;
+
+    while (cents > 24)
+    {
+    // Subtract the value of those dimes from cents
+        cents -= 10;
+        dimes ++;
+    }
+    return dimes;
+}
 
     // Calculate how many nickels you should give customer
+int calculate_nickels(int cents)
+{
+    int nickels = 0;
+
     // Subtract the value of those nickels from remaining cents
+    while (cents > 5)
+    {
+        cents -= 5;
+        nickels ++;
+    }
+    return nickels;
+}
 
     // Calculate how many pennies you should give customer
+int calculate_penies(int cents)
+{
+    int pennies = 0;
+
     // Subtract the value of those pennies from remaining cents
+    while (cents > 1)
+    {
+        cents -= 1;
+        pennies ++;
+    }
+    return pennies;
+}
 
     // Sum the number of quarters, dimes, nickels, and pennies used
+int calculate_sum(int quarters, int dimes, int nickels, int pennies)
+{
     // Print that sum
-
+    int sum = quarters + dimes + nickels + pennies;
+    printf("%i\n", sum);
+}
