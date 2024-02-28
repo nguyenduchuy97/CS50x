695453aa599a5937cad662d232d0a3d5fcecfe80
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-02-28 07:54:32 +0000

    Wed, Feb 28, 2024, 2:54 PM +07:00

--- credit/credit.c
+++ credit/credit.c
@@ -4,10 +4,10 @@
 
 int main()
 {
-    long int card = get_long("Number: ");
-    string s_card = card;
-    printf("%ld\n", card);
-    printf("%s\n", strlen(s_card))
+    string s_card = get_long("Number: ");
+    long int card = (long int) s_card;
+    printf("This is int %ld\n", card);
+    printf("This is strings %s\n", strlen(s_card))
 }
 
 // Start from the second-to-last digit of the credit card number.
