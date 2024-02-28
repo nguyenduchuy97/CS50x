472437a13da18a414486f83a2963c8c90703a62d
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-02-28 02:53:25 +0000

    Wed, Feb 28, 2024, 9:53 AM +07:00

---
+++ cash/cash.c
@@ -0,0 +1,27 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // Prompt the user for change owed, in cents
+    int c_owed;
+    do
+    {
+        c_owed = get_int("Change owed: ");
+    }
+    while(c_owed <0);
+    // Calculate how many quarters you should give customer
+    // Subtract the value of those quarters from cents
+
+    // Calculate how many dimes you should give customer
+    // Subtract the value of those dimes from remaining cents
+
+    // Calculate how many nickels you should give customer
+    // Subtract the value of those nickels from remaining cents
+
+    // Calculate how many pennies you should give customer
+    // Subtract the value of those pennies from remaining cents
+
+    // Sum the number of quarters, dimes, nickels, and pennies used
+    // Print that sum
+}
