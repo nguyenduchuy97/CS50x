2ad99cb87ad67e80315df981de983b95fcc1a0cb
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-02-28 07:31:10 +0000

    Wed, Feb 28, 2024, 2:31 PM +07:00

---
+++ credit/credit.c
@@ -0,0 +1,9 @@
+#include <cs50.h>
+#include <stdio.h>
+
+
+// Start from the second-to-last digit of the credit card number.
+// Double every other digit, moving from right to left.
+// If doubling a digit results in a two-digit number, add the individual digits of that product together.
+// Sum all the obtained digits from the doubling process along with the digits that were not doubled.
+// If the total sum ends in 0 (i.e., it is divisible by 10), then the credit card number is considered valid.
