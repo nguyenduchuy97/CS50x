78b7daa1037cb92dd07b1502cc4d8cd5edfdea39
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-02-28 08:09:59 +0000

    Wed, Feb 28, 2024, 3:09 PM +07:00

--- credit/credit.c
+++ credit/credit.c
@@ -6,7 +6,7 @@ int main()
 {
     long int card = get_long("Number: ");
     char s_card[32];
-    sprintf(s_card, "%li", card);
+    sprintf(s_card, "%ld", card);
 
     printf("This is int %ld\n", card);
     printf("This is strings %s\n", s_card);
