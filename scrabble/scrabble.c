bedc180801e7bf28f67676a839f28349c97b1440
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-02-29 11:58:11 +0000

    Thu, Feb 29, 2024, 6:58 PM +07:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -19,7 +19,7 @@ int compute_score(string player)
 {
     string alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; //{"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
     int points[] = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
-    int sum;
+    int sum = 0;
     int len = strlen(player);
     char up_l[len];
     int n = 0;
@@ -29,12 +29,21 @@ int compute_score(string player)
         up_l[i] = toupper(player[i]);
         while (n < 26)
         {
-            if ((up_l[i] - alpha[n] > 25))
+            if ((up_l[i] - alpha[n] < 25 && up_l[i] - alpha[n] > 0))
             {
-                sum = 0;
-                n++;
+                if ((up_l[i] - alpha[n] != 0))
+                {
+                    n++;
+                }
+                else
+                {
+                    sum += points[i];
+                }
             }
-
+            else
+            {
+                sum += 0;
+            }
         }
 
     }
@@ -46,6 +55,6 @@ int compute_score(string player)
         //n++;
     //}
 
-    return 0;
+    return sum;
 }
 
