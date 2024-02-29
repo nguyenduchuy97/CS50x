0788c9429c1b67a82b77576cf7116e48f7a7283f
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-02-29 12:48:35 +0000

    Thu, Feb 29, 2024, 7:48 PM +07:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -19,7 +19,7 @@ int main(void)
 
 int compute_score(string player)
 {
-    string alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; //{"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+    string al = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; //{"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
     int points[] = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
     int sum = 0;
     int len = strlen(player);
@@ -32,7 +32,15 @@ int compute_score(string player)
 
         for (int n = 0; n < 26; n++)
         {
-            
+            if ((up[i] - al[n]) == 0)
+            {
+                printf("UP: %c, AL: %c, -: %i\n", up[i], al[n], up[i] - al[n]);
+                sum += points[n];
+            }
+            else
+            {
+                sum += 0;
+            }
         }
     }
 
