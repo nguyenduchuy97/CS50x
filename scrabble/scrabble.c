c4241d98274671ce1736a45549a5da8ab183a592
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-02-29 09:26:00 +0000

    Thu, Feb 29, 2024, 4:26 PM +07:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -7,10 +7,6 @@ int compute_score(string player);
 
 int main(void)
 {
-    const int N = 26;
-    char alpha[N] = {A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z};
-    int points[N] = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
-
     string pl_1 = get_string("Player 1: ");
     string pl_2 = get_string("Player 2: ");
 
@@ -21,9 +17,9 @@ int main(void)
 
 int compute_score(string player)
 {
-    const int N = 26;
-    char alpha[N] = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
-    int points[N] = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
+    const int N = 27;
+    string alpha = ""//{"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+    int points[] = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
     string cap_letters;
     int len = strlen(player);
 
@@ -31,4 +27,5 @@ int compute_score(string player)
     {
         cap_letters = toupper(pl_1[i]);
         printf("%s", cap_letters);
+}
 }
