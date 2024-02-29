4c6037b8a13718d5eb807dcdafe58df10d3d4662
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-02-29 01:41:47 +0000

    Thu, Feb 29, 2024, 8:41 AM +07:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,7 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
 
-bool check_valid(long int card);
+bool check_sum(long int card);
 string id_issuer(long int card);
 
 int main()
@@ -10,9 +10,8 @@ int main()
 
     if (check_valid(number))
     {
-        //string issuer = id_issuer(number);
-        //printf("%s\n", issuer);
-        //check_valid(number);
+        string issuer = id_issuer(number);
+        printf("%s\n", issuer);
     }
     else
     {
@@ -20,7 +19,7 @@ int main()
     }
  }
 
-bool check_valid(long int card)
+bool check_sum(long int card)
 {
     int length = 0;
     int sum = 0;
@@ -34,61 +33,43 @@ bool check_valid(long int card)
         temp_card /= 10;
     }
 
+    //  If the number of digit odd (count the last) or even (won't count the last), because we count from right to left.
+    double_step = length % 2 == 0;
+
+    while (card > 0)
+    {
+        // Start from the second-to-last digit of the credit card number.
+        long int digit = card % 10;
+        // Double every other digit, moving from right to left.
+        if (double_step)
+        {
+            digit *= 2;
+            // If doubling a digit results in a two-digit number, add the individual digits of that product together.
+            if (digit > 9)
+            {
+                digit = digit % 10 + digit / 10;
+            }
+        }
+        // Sum all the obtained digits from the doubling process along with the digits that were not doubled.
+        sum += digit;
+        // Move to the next 2 digit.
+        card /= 10;
+        // Change to true of false so it will jump 2 step at a time.
+        double_step = !double_step;
+    }
+    // Check if the last digit of the algorithm's result equal 0 and the number of digit between 13 to 16.
+    // If the total sum ends in 0 (i.e., it is divisible by 10), then the credit card number is considered valid.
+    if ((sum % 10 == 0) && (length >= 13 && length <= 16))
+    {
+        printf("length: %i, sum: %i\n", length, sum);
+        return true;
+    }
     else
     {
-        double_step = length % 2 == 0;
-
-        while (card > 0)
-        {
-            // Start from the second-to-last digit of the credit card number.
-            long int digit = card % 10;
-            // Double every other digit, moving from right to left.
-            if (double_step)
-            {
-                digit *= 2;
-                // If doubling a digit results in a two-digit number, add the individual digits of that product together.
-                if (digit > 9)
-                {
-                    digit = digit % 10 + digit / 10;
-                }
-            }
-            // Sum all the obtained digits from the doubling process along with the digits that were not doubled.
-            sum += digit;
-            // Move to the next 2 digit.
-            card /= 10;
-            // Change to true of false so it will jump 2 step at a time.
-            double_step = !double_step;
-        }
-        // Check if the last digit of the algorithm's result equal 0 and the number of digit between 13 to 16.
-        // If the total sum ends in 0 (i.e., it is divisible by 10), then the credit card number is considered valid.
-        if ((sum % 10 == 0) && (length >= 13 && length <= 16))
-        {
-            if ((length == 15 && first_2_d == 34) || (length == 15 && first_2_d == 37))
-            {
-                printf("AMEX\n");
-                return true;
-            }
-
-            else if ((length == 16) && (first_2_d >= 51 && first_2_d <= 55))
-            {
-                printf("MASTERCARD\n");
-                return true;
-            }
-
-            else if ((length == 13 || length == 16) && (first_2_d >= 40 && first_2_d <= 49))
-            {
-                printf("VISA\n");
-                return true;
-            }
-            return true;
-        }
-        else
-        {
-            printf("INVALID\n");
-            return false;
-        }
-        //return (sum % 10 == 0) && (length >= 13 && length <= 16);
+        printf("length: %i, sum: %i", length, sum);
+        return false;
     }
+    //return (sum % 10 == 0) && (length >= 13 && length <= 16);
 }
 
 string id_issuer(long int card)
@@ -101,6 +82,7 @@ string id_issuer(long int card)
         length++;
         temp_card /= 10;
     }
+    printf("This is the first 2 digit: %li and length: %i\n", temp_card, length);
 
     // Get the first two digits of the card number.
     long int first_2_d = card;
