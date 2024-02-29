8cbec89b9e3686ef26d0430ae422857da4c25e3e
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-02-29 10:52:04 +0000

    Thu, Feb 29, 2024, 5:52 PM +07:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -19,9 +19,14 @@ int compute_score(string player)
 {
     string alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; //{"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
     int points[] = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
-    string cap_letters;
+    string up_l;
     int len = strlen(player);
-    printf("%i\n", len);
+    for (int i = 0; i < len; i++)
+    {
+        up_l = player[i];
+
+    }
+    printf("%s\n", up_l);
 
     return 0;
 }
