3dedb63e345115d3df3e2c70b71bc7f985a60bf1
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-02-29 13:29:58 +0000

    Thu, Feb 29, 2024, 8:29 PM +07:00

---
+++ readability/readability.c
@@ -0,0 +1,6 @@
+
+
+
+
+// Fomula: index = 0.0588 * L - 0.296 * S - 15.8
+// where L is the average number of letters per 100 words in the text, and S is the average number of sentences per 100 words in the text.
