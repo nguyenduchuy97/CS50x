6cf23bd4f87172f7c7db7d8b63a0c036abf40758
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-02-29 09:17:21 +0000

    Thu, Feb 29, 2024, 4:17 PM +07:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <string.h>
 
+int compute_score(string player);
+
 int main(void)
 {
     const int N = 26;
@@ -11,6 +13,18 @@ int main(void)
 
     string pl_1 = get_string("Player 1: ");
     string pl_2 = get_string("Player 2: ");
+
+    int score1 = compute_score(pl_1);
+    int score2 = compute_score(pl_2);
+
+}
+
+int compute_score(string player)
+{
+    const int N = 26;
+    char alpha[N] = {A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z};
+    int points[N] = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
+    
     int len_pl1 = strlen(pl_1);
     int len_pl2 = strlen(pl_2);
 
@@ -22,6 +36,4 @@ int main(void)
     {
         toupper(pl_2[i]);
     }
-
-
 }
