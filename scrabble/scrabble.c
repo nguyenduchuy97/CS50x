5a8bf8e25d325557ce80db06830055879221c19e
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-02-29 09:23:31 +0000

    Thu, Feb 29, 2024, 4:23 PM +07:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -22,7 +22,7 @@ int main(void)
 int compute_score(string player)
 {
     const int N = 26;
-    char alpha[N] = {A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z};
+    char alpha[N] = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
     int points[N] = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
     string cap_letters;
     int len = strlen(player);
