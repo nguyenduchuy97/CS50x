b7506ac2510c39cce1294417b2391dee5d07c5cf
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-02-29 08:43:16 +0000

    Thu, Feb 29, 2024, 3:43 PM +07:00

---
+++ scrabble/scrabble.c
@@ -0,0 +1,11 @@
+#include <ctype.h>
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    string pl_1 = get_string("Player 1: ");
+    string pl_2 = get_string("Player 2: ");
+    
+}
