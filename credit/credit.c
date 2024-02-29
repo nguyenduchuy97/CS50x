f171038fe4950176afa7061adb5e7e4ff36c7773
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-02-29 01:22:56 +0000

    Thu, Feb 29, 2024, 8:22 AM +07:00

--- credit/credit.c
+++ credit/credit.c
@@ -53,49 +53,47 @@ long int first_2_d = card;
     {
         return "VISA";
     }
-    else
-    {
-        return "INVALID";
-    }
-
     //-------------------------------------------------------------------------------------------------------------
     //  If the number of digit odd (count the last) or even (won't count the last), because we count from right to left.
-    double_step = length % 2 == 0;
-
-    while (card > 0)
-    {
-        // Start from the second-to-last digit of the credit card number.
-        long int digit = card % 10;
-        // Double every other digit, moving from right to left.
-        if (double_step)
-        {
-            digit *= 2;
-            // If doubling a digit results in a two-digit number, add the individual digits of that product together.
-            if (digit > 9)
-            {
-                digit = digit % 10 + digit / 10;
-            }
-        }
-        // Sum all the obtained digits from the doubling process along with the digits that were not doubled.
-        sum += digit;
-        // Move to the next 2 digit.
-        card /= 10;
-        // Change to true of false so it will jump 2 step at a time.
-        double_step = !double_step;
-    }
-    // Check if the last digit of the algorithm's result equal 0 and the number of digit between 13 to 16.
-    // If the total sum ends in 0 (i.e., it is divisible by 10), then the credit card number is considered valid.
-    if ((sum % 10 == 0) && (length >= 13 && length <= 16))
-    {
-        printf("length: %i, sum: %i\n", length, sum);
-        return true;
-    }
     else
     {
-        printf("length: %i, sum: %i\n", length, sum);
-        return false;
+        double_step = length % 2 == 0;
+
+        while (card > 0)
+        {
+            // Start from the second-to-last digit of the credit card number.
+            long int digit = card % 10;
+            // Double every other digit, moving from right to left.
+            if (double_step)
+            {
+                digit *= 2;
+                // If doubling a digit results in a two-digit number, add the individual digits of that product together.
+                if (digit > 9)
+                {
+                    digit = digit % 10 + digit / 10;
+                }
+            }
+            // Sum all the obtained digits from the doubling process along with the digits that were not doubled.
+            sum += digit;
+            // Move to the next 2 digit.
+            card /= 10;
+            // Change to true of false so it will jump 2 step at a time.
+            double_step = !double_step;
+        }
+        // Check if the last digit of the algorithm's result equal 0 and the number of digit between 13 to 16.
+        // If the total sum ends in 0 (i.e., it is divisible by 10), then the credit card number is considered valid.
+        if ((sum % 10 == 0) && (length >= 13 && length <= 16))
+        {
+            printf("length: %i, sum: %i\n", length, sum);
+            return true;
+        }
+        else
+        {
+            printf("length: %i, sum: %i\n", length, sum);
+            return false;
+        }
+        //return (sum % 10 == 0) && (length >= 13 && length <= 16);
     }
-    //return (sum % 10 == 0) && (length >= 13 && length <= 16);
 }
 
 string id_issuer(long int card)
