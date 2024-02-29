70f92b49f2ed60e6f7b8bffb9be3446f97fde6ae
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-02-29 09:13:09 +0000

    Thu, Feb 29, 2024, 4:13 PM +07:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -5,8 +5,9 @@
 
 int main(void)
 {
-    char alpha[26] = {};
-    int point[26] = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
+    const int N = 26;
+    char alpha[N] = {A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z};
+    int points[N] = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
 
     string pl_1 = get_string("Player 1: ");
     string pl_2 = get_string("Player 2: ");
