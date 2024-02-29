a1c22da22946efada96154d2f737c34a334a41c1
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-02-29 15:23:02 +0000

    Thu, Feb 29, 2024, 10:23 PM +07:00

--- readability/readability.c
+++ readability/readability.c
@@ -26,10 +26,11 @@ int main(void)
 		{
 			words += 1;
 		}
-        else if (len -1 == i)
+        else if (ispunct(input[len]))
 			{
                 printf("Plus last word\n");
-				words +=1;
+				words += 1;
+                sentences += 1;
 			}
 		// Find letters
 		else if (isalnum(input[i]))
