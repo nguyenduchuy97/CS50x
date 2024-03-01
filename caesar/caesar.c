ed4577fa5a1f13c55a4d03ee690f275f9ddc94a8
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-03-01 12:37:02 +0000

    Fri, Mar 1, 2024, 7:37 PM +07:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -2,18 +2,27 @@
 #include <stdio.h>
 #include <ctype.h>
 
+char rotate(char c, int n);
+
 int main(int argc, char argv[])
 {
     int k = ;
     if (argc == 2 && isdigit(argv[1]) && (int) argv[1] >= 0)
     {
         k = (int) argv[1];
-        string plain = get_string("Plaintext:  ");
+        string p = get_string("Plaintext:  ");
+        int len = strlen(p)
     }
     else
     {
         printf("Usage: %s key\n", argv[0]);
         return 1;
     }
+
+
+}
+
+char rotate(char c, int n)
+{
 
 }
