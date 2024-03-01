0185f806b56b78cf5283d90371d142f76284a116
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-03-01 15:38:42 +0000

    Fri, Mar 1, 2024, 10:38 PM +07:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -8,25 +8,30 @@ char rotate(char c, string n);
 
 int main(int argc, string argv[])
 {
+    // Only accept 2 arguments
     if (argc != 2)
     {
         printf("Usage %s key\n", argv[0]);
         return 1;
     }
     int len_k = strlen(argv[1]);
+
     for (int x = 0; x < len_k; x++)
     {
+        // Only accept 26 characters and alphabet characters.
         if (!isalpha(argv[1][x]) || len_k != 26)
         {
             printf("The key must contains 26 characters.\n");
             return 1;
         }
     }
+    // Input plain text.
     string p = get_string("plaintext:  ");
     int len_p = strlen(p);
 
     for (int i = 0; i < len_p; i++)
     {
+        // Substitution process with the key
         p[i] = rotate(p[i],argv[1]);
     }
 
