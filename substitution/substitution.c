37fba66ed2fa1d4bbcdd7fc20fe68f0f9752ff0c
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-03-01 15:13:28 +0000

    Fri, Mar 1, 2024, 10:13 PM +07:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -44,12 +44,12 @@ char rotate(char c, string n)
         for (int i = 0; i < strlen(n); i++)
             if (isupper(c))
             {
-                x = (c - 'A') % 26 + i;
+                x = (c - 'A' + i) % 26 ;
                 c = n[x];
             }
             else
             {
-                x = (c - 'a') % 26 + i;
+                x = (c - 'a' + i) % 26;
                 c = n[x];
             }
         return c;
