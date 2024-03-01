7320769badf4f6d7dfdd32dab60ee9ea3b968dc6
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-03-01 12:13:42 +0000

    Fri, Mar 1, 2024, 7:13 PM +07:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,7 +1,15 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <ctype.h>
 
 int main(int argc, char argv[])
 {
-    
+    if (argc == 2 && argv[1])
+    {
+
+    }
+    else
+    {
+        printf("Usage: %s key\n", argv[0]);
+    }
 }
