00c70af0cb7d3c7b83345cb530cc20e7bfd12cd8
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-03-01 14:32:40 +0000

    Fri, Mar 1, 2024, 9:32 PM +07:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -9,6 +9,14 @@ int main(int argc, string argv[])
     if (argc != 2)
     (
         printf("Usage %s key\n", argv[0]);
+        return 1;
     )
-    if (argv[1] )
+    int len = strlen(argv[1]);
+    for (int i = 0; i < len; i++)
+    {
+        if (!isalpha(argv[1][i]))
+        {
+            
+        }
+    }
 }
