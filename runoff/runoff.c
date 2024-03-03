6a0b9b5d1519e2c7466a3bd40c872563f53a7a14
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-03-03 14:14:11 +0000

    Sun, Mar 3, 2024, 9:14 PM +07:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -187,7 +187,15 @@ int find_min(void)
 // Return true if the election is tied between all candidates, false otherwise
 bool is_tie(int min)
 {
-    // TODO
+    int sum = 0;
+    for (int i = 0; i < candidate_count; i++)
+    {
+        sum += candidates[i].votes
+    }
+    if (sum/candidate_count == min)
+    {
+        return true;
+    }
     return false;
 }
 
