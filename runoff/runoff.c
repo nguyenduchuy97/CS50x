0a9319fdd4174833ba4c7b5b0744871f7d321ee3
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-03-03 14:24:46 +0000

    Sun, Mar 3, 2024, 9:24 PM +07:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 // Max voters and candidates
 #define MAX_VOTERS 100
@@ -128,9 +129,9 @@ bool vote(int voter, int rank, string name)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        if (strcmp(name, candidate[i].name) == 0)
+        if (strcmp(name, candidates[i].name) == 0)
         {
-            candidate[i].votes++;
+            candidates[i].votes++;
             preferences[voter][rank] = i;
             return true;
         }
