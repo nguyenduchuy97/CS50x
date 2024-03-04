0c567ad3af599403b77bf35c9dfe2e564788f952
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-03-04 07:25:54 +0000

    Mon, Mar 4, 2024, 2:25 PM +07:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -98,7 +98,14 @@ int main(int argc, string argv[])
 // Update ranks given a new vote
 bool vote(int rank, string name, int ranks[])
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (strcmp(name, candidates[i]) == 0)
+        {
+            ranks[i] = rank;
+            return true;
+        }
+    }
     return false;
 }
 
