4591d128be6f3c0207de37ee3175e64820a78d95
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-03-05 13:38:36 +0000

    Tue, Mar 5, 2024, 8:38 PM +07:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -130,7 +130,6 @@ void record_preferences(int ranks[])
 // Record pairs of candidates where one is preferred over the other
 void add_pairs(void)
 {
-    pair_count = 0;
     for (int i = 0; i < candidate_count; i++)
     {
         for (int j = 0; j < candidate_count; j++)
