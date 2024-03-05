5373c8bd66e2133710b68af331352da5e7980aa0
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-03-05 07:05:04 +0000

    Tue, Mar 5, 2024, 2:05 PM +07:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -172,7 +172,13 @@ void sort_pairs(void)
 // Lock pairs into the candidate graph in order, without creating cycles
 void lock_pairs(void)
 {
-    // TODO
+    for (int i = 0; i < pair_count; i++)
+    {
+        if (pairs[i] < pairs[pair_count]winner)
+        {
+            locked[pairs[i]winner][pairs[i]winner] = true;
+        }
+    }
     return;
 }
 
