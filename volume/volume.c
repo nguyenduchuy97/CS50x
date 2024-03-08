451ef62eb5379190f713d035b02cfc6dd6fe5695
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-03-08 09:00:19 +0000

    Fri, Mar 8, 2024, 4:00 PM +07:00

--- volume/volume.c
+++ volume/volume.c
@@ -38,11 +38,11 @@ int main(int argc, char *argv[])
     fread(header, HEADER_SIZE, 1, input);
     fwrite(header, HEADER_SIZE, 1, output);
     // TODO: Read samples from input file and write updated data to output file
-    uint16_t buffer;
-    while (fread(&buffer, sizeof(uint16_t), 1, input) != 0)
+    int16_t buffer;
+    while (fread(&buffer, sizeof(int16_t), 1, input) != 0)
     {
         buffer *= factor;
-        fwrite(&buffer, sizeof(uint16_t), 1, output);
+        fwrite(&buffer, sizeof(int16_t), 1, output);
     }
     // Close files
     fclose(input);
