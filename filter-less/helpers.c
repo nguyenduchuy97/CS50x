263e1740fe36cdbb241d371d4bc69b71e85154d2
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-03-09 08:46:32 +0000

    Sat, Mar 9, 2024, 3:46 PM +07:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -1,16 +1,17 @@
 #include "helpers.h"
+#include <math.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    double new;
+    int new;
     // Loop over all pixels
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
             // Take average of red, green, and blue
-            new = round(image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue)
+            new = round((image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3.0)
 
             // Update pixel values
         }
