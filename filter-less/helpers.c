d446a62f5c30567a17fd831d5caea2a108b19a39
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-03-09 12:33:03 +0000

    Sat, Mar 9, 2024, 7:33 PM +07:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -81,7 +81,13 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 {
     // Create a copy of image
     RGBTRIPLE copy[height][width];
-    
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            copy[i][j] = image[i][j];
+        }
+    }
 
     return;
 }
