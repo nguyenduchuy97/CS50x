95dbda9bce0a36137f7c3ddc7ef64d79441d6d32
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-03-09 09:13:53 +0000

    Sat, Mar 9, 2024, 4:13 PM +07:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -33,7 +33,7 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             sepiaRed = round(.393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue);
             sepiaGreen = round(.349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue);
             sepiaBlue = round(.272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue);
-            
+
             if (sepiaRed > 255)
             {
                 sepiaRed = 255;
@@ -48,6 +48,7 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             }
 
             // Update pixel with sepia values
+            image[i][j] = 
         }
     }
     return;
