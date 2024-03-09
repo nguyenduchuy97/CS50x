cfd9689966cfd7cc868a6c2c5b57466045c79c81
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-03-09 08:41:58 +0000

    Sat, Mar 9, 2024, 3:41 PM +07:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -3,14 +3,14 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    double new;
     // Loop over all pixels
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
             // Take average of red, green, and blue
-            
-
+            new = image[i][j].rgbtRed
 
             // Update pixel values
         }
