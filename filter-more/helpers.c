902ae926a8a0c610fdf204eb636bb3fe64f80510
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-03-10 12:16:18 +0000

    Sun, Mar 10, 2024, 7:16 PM +07:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -4,11 +4,15 @@
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
     int avg;
+    // Loop over all pixels.
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
+            // Take average of red, green, and blue
             avg = round((image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3.0);
+
+            // Update pixel values
             image[i][j].rgbtRed = avg;
             image[i][j].rgbtGreen = avg;
             image[i][j].rgbtBlue = avg;
@@ -21,10 +25,12 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
     RGBTRIPLE swap;
+    // Loop over all pixels
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width / 2; j++)
         {
+            // Swap pixels
             swap[i][j] = image[i][j];
             image[i][j] = image[i][width - 1 - j];
             image[i][width - 1 - j] = swap[i][j];
@@ -37,13 +43,15 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
     RGBTRIPLE copy;
+    // Create a copy of image.
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            
+            copy[i][j] = image[i][j];
         }
     }
+    
     return;
 }
 
