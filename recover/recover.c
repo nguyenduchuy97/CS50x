57160f9f9514959f5e15840d0cdf1badb14ef7b4
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-03-12 13:12:30 +0000

    Tue, Mar 12, 2024, 8:12 PM +07:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main(int argc, char *argv[])
 {
- 
+    if (argc != 2)
+    {
+        printf("Usage: %s filename\n", argv[0]);
+        return 1;
+    }
+
+    FILE *f = fopen(argv[1], 'r');
+    
 }
