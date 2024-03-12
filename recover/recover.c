da2b41c470b6c57f1773ae3a61323d0bf0abb601
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-03-12 13:17:50 +0000

    Tue, Mar 12, 2024, 8:17 PM +07:00

--- recover/recover.c
+++ recover/recover.c
@@ -3,13 +3,21 @@
 
 int main(int argc, char *argv[])
 {
+    // Accept a single command-line argument
     if (argc != 2)
     {
         printf("Usage: %s filename\n", argv[0]);
         return 1;
     }
-
+    // Open the memory card
     FILE *f = fopen(argv[1], 'r');
 
-    
+    // Create a buffer for a block of data
+    uint8_t buffer[512];
+
+    // While there's still data left to read from the memory card
+    while (fread(buffer, 1, 512, f) == 512)
+    {
+        
+    }
 }
