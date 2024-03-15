95f3124ef4c68cf029419ca9c59d36eef4567592
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-03-15 12:42:25 +0000

    Fri, Mar 15, 2024, 7:42 PM +07:00

--- recover/recover.c
+++ recover/recover.c
@@ -22,7 +22,8 @@ int main(int argc, char *argv[])
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && buffer[3] & 0xf0 == 0xe0 )
         {
             // If first JPEG file
-            FILE *r = fread(filename, 1, 512, buffer)
+            FILE *r = fread(filename, 1, 512, buffer);
+            FILE *w = fwrite()
         }
 
     }
