4d3eee2f320fa228945e8a30ea269176f36d6dc1
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-03-15 13:28:36 +0000

    Fri, Mar 15, 2024, 8:28 PM +07:00

--- recover/recover.c
+++ recover/recover.c
@@ -14,17 +14,18 @@ int main(int argc, char *argv[])
 
     // Create a buffer for a block of data
     uint8_t buffer[512];
-
+    int i = 0;
+    char *num[7];
     // While there's still data left to read from the memory card
     while (fread(buffer, 1, 512, f) == 512)
     {
         // If start of a new JPEG file
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && buffer[3] & 0xf0 == 0xe0 )
         {
-            int i = 1;
             // If first JPEG file
-            FILE *file = fwrite(buffer, 1, 512, sprintf())
-            FILE *w = fwrite(buff, 1, 512, )
+            FILE *file = fopen(buffer, 1, 512, sprintf(num, "%03i", i))
+            i++;
+            fwrite(buffer, 1, 512, sprintf(num, "%03i", i))
         }
 
     }
