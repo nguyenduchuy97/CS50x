8ce11e4de2ced5ccdfc49f00813b5414e7c6cef1
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-03-19 12:43:25 +0000

    Tue, Mar 19, 2024, 7:43 PM +07:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -65,8 +65,8 @@ person *create_family(int generations)
         new_person->parents[1] = NULL;
 
         // TODO: Randomly assign alleles
-        new_person->alleles[0] = rand() % 2;
-        new_person->alleles[1] = rand() % 2;
+        new_person->alleles[0] = random_allele();
+        new_person->alleles[1] = random_allele();
     }
 
     // TODO: Return newly created person
