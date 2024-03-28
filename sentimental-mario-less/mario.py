d48ecd757af43a84c453dafb761f2ce148b0d7d1
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-03-28 13:44:26 +0000

    Thu, Mar 28, 2024, 8:44 PM +07:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -15,10 +15,7 @@ def main():
 
     for i in range(height):
         for j in range(height):
-            if i == 0 or j == 0:
-                print("", end="")
-            else:
-                print((height - j) * " ", end="")
+            print((height - j) * " ", end="")
         print((i + 1) * "#")
 
 
