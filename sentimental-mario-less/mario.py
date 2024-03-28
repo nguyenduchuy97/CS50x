239045ac1d6bc157f8dc8ecaf21dd42d0adc33be
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-03-28 13:41:17 +0000

    Thu, Mar 28, 2024, 8:41 PM +07:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,6 +1,6 @@
 
 def pyramid(space, brick):
-    
+
 
 
 
@@ -16,6 +16,7 @@ def main():
 
     for i in range(height):
         for j in range(height):
+            if 
             print((height - j) * " ", end="")
         print((i + 1) * "#")
 
