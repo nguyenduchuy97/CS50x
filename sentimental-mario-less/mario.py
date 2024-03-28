5d4dd170f886c2ceab59e1114184249ec6e45425
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-03-28 13:48:43 +0000

    Thu, Mar 28, 2024, 8:48 PM +07:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,24 +1,13 @@
+height = 0
+while True:
+    height = int(input("Height: "))
+    if height in range(1,9):
+        break
 
+for i in range(height):
+    for j in range(height):
+        print("", end="")
+    print((i + 1) * "#")
 
-
-
-
-
-
-
-def main():
-    height = 0
-    while True:
-        height = int(input("Height: "))
-        if height in range(1,9):
-            break
-
-    for i in range(height):
-        for j in range(height):
-            print((height - i) * " ", end="")
-        print((i + 1) * "#")
-
-
-main()
 
 
