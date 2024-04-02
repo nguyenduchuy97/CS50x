c9a7a24f77a60bbbb3b842a37e6f029cc3a46cf4
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-04-02 10:04:48 +0000

    Tue, Apr 2, 2024, 5:04 PM +07:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -6,37 +6,3 @@ while True:
 
     if cents >= 0:
         break
-
-def quaters(cent):
-    quater = 0
-    while cent > 24:
-        cent -= 25.0
-        quater += 1
-    return quater
-
-def dimes(cent):
-    dime = 0
-    while cent > 9:
-        cent -= 10.0
-        dime += 1
-    return dime
-
-def nickels(cent):
-    nickel = 0
-    while cent > 4:
-        cent -= 5.0
-        nickel += 1
-    return nickel
-
-def pennies(cent):
-    pennie = 0
-    while cent > 0:
-        cent -= 1.0
-        pennie += 1
-    return pennie
-
-def main():
-    quater = quaters(cent)
-    dime = dimes(cent)
-    nickel = nickels(cent)
-    pennie = pennies(cent)
