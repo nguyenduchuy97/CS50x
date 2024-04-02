3f909d3218fbf6fe533b8adc3b9c2fdc02ec054c
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-04-02 07:31:40 +0000

    Tue, Apr 2, 2024, 2:31 PM +07:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -4,8 +4,9 @@ while True:
     try:
         # Get input
         height = input("Height: ")
+
         # Take the positive integer between 1 to 8
-        if height in range(1,9):
+        if height in range(1, 9):
             break
     except:
         continue
