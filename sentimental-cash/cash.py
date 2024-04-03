0654b79f350c4f559c5ed52715b9e2589f8f7309
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-04-03 00:50:39 +0000

    Wed, Apr 3, 2024, 7:50 AM +07:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,8 +1,24 @@
 from cs50 import get_float
 
+coins = 0
 cents = 0.0
 while True:
     cents = get_float("Change owed: ")
 
     if cents >= 0:
         break
+
+while cents > 0:
+    if cents > 24:
+        cents -= 25
+        coins += 1
+    elif cents > 9:
+        cents -= 10
+        coins += 1
+    elif cents > 4:
+        cents -= 5
+        coins += 1
+    elif cents > 0:
+        cents -= 1
+        coins += 1
+
