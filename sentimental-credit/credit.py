59c2a362fc9b08b6a2f0bd94107194062030221f
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-04-06 09:45:57 +0000

    Sat, Apr 6, 2024, 4:45 PM +07:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -4,6 +4,8 @@ import re
 sum = 0
 length = 0
 second_digit = 0
+digit = 0
+booll = False
 
 number = get_string("Number: ")
 length = len(number)
@@ -13,5 +15,12 @@ while number > 99:
     second_digit = round(int(number) / 10)
 
 while number > 0:
-    
+    number %= 10
+
+    if booll is True:
+        digit *= 2
+
+        if digit > 9:
+            digit = digit % 10 + round(digit / 10)
+
 
