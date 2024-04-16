de902a6ad318034d98bbca0a3e87f133222fff39
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-04-16 04:30:44 +0000

    Tue, Apr 16, 2024, 11:30 AM +07:00

--- dna/dna.py
+++ dna/dna.py
@@ -22,7 +22,8 @@ def main():
         seq = dna.read()
 
     # TODO: Find longest match of each STR in DNA sequence
-
+    for i in range(len(rows)):
+        num = longest_match(seq, rows[i])
     # TODO: Check database for matching profiles
 
     return
