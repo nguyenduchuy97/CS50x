e368f303bea02fa5fd7204eb9d8b7e99d2995c3e
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-04-17 04:31:33 +0000

    Wed, Apr 17, 2024, 11:31 AM +07:00

--- dna/dna.py
+++ dna/dna.py
@@ -30,11 +30,6 @@ def main():
 
     # TODO: Check database for matching profiles
     for i in range(len(rows)):
-        for j in range(len(rows)):
-            if num[j] != rows[i][lst[j]]:
-                break
-            print(num[j] == rows[i][lst[j]])
-            print(rows[i]["name"])
 
     print("No match")
 
