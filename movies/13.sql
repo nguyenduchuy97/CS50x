3721defc01faa997072a35cd82a6ddc002952b42
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-04-23 12:55:03 +0000

    Tue, Apr 23, 2024, 7:55 PM +07:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,10 +1,12 @@
 SELECT name
 FROM people
-JOIN stars
-ON people.id = stars.person_id
-JOIN movies
-ON movies.id = stars.movie_id
-JOIN ratings
-ON ratings.movie_id = movies.id
-WHERE ratings.rating = movies.id
-AND people.name
+WHERE id =
+(
+    SELECT person_id
+    FROM stars
+    WHERE movie_id =
+    (
+        SELECT movie_id
+        FROM ratings
+    )
+)
