e4b47912f0964fdd0c3148d2829215c00bb93fc7
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-04-23 12:21:41 +0000

    Tue, Apr 23, 2024, 7:21 PM +07:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,2 +1,14 @@
 SELECT title
 FROM movies
+WHERE id =
+(
+    SELECT person_id
+    FROM stars
+    WHERE
+    (
+    SELECT name
+    FROM people
+    WHERE name = "Bradley Cooper"
+    OR name = "Jennifer Lawrence"
+    )
+)
