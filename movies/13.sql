b2edec571cadb7287cb64518a2a9ccaabf9b809f
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-04-23 13:29:51 +0000

    Tue, Apr 23, 2024, 8:29 PM +07:00

--- movies/13.sql
+++ movies/13.sql
@@ -6,14 +6,19 @@ WHERE id =
     FROM stars
     WHERE movie_id =
     (
-        SELECT movie_id
-        FROM stars
-        WHERE person_id =
+        SELECT id
+        FROM movies
+        WHERE id =
         (
-            SELECT id
-            FROM people
-            WHERE name = "Kevin Bacon"
-            AND birth = "1958"
+            SELECT movie_id
+            FROM stars
+            WHERE person_id =
+            (
+                SELECT id
+                FROM people
+                WHERE name = "Kevin Bacon"
+                AND birth = "1958"
+            )
         )
     )
 )
