7007f8cf11e2d8c1523fafd154e1641375e3f180
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-04-23 13:52:17 +0000

    Tue, Apr 23, 2024, 8:52 PM +07:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,14 +1,14 @@
 SELECT title
 FROM movies
-WHERE id =
+WHERE id IN
 (
     SELECT movie_id
     FROM stars
     WHERE person_id =
     (
-    SELECT id
-    FROM people
-    WHERE name = "Bradley Cooper"
+        SELECT id
+        FROM people
+        WHERE name = "Bradley Cooper"
     )
     AND person_id =
     (
