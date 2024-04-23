9624983a9f3df1c50c474543d1ee318af235f530
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-04-23 12:04:34 +0000

    Tue, Apr 23, 2024, 7:04 PM +07:00

--- movies/10.sql
+++ movies/10.sql
@@ -5,4 +5,6 @@ ON people.id = directors.person_id
 JOIN movies
 ON movies.id = directors.movie_id
 JOIN ratings
-ON ratings.
+ON ratings.movie_id = movies.id
+WHERE ratings.rating >= 9.0
+AND directors.person_id = people.id;
