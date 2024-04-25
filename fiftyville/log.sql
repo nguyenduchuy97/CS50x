887c9ad4cfdd281460d450c23686321b55b1a324
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-04-25 10:00:27 +0000

    Thu, Apr 25, 2024, 5:00 PM +07:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,7 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+-- Find the ID of the airport where the original place of the theft.
+SELECT id, abbreviation, full_name, city FROM airports;
+
+-- Find the origin airport
+SELECT * FROM flights WHERE origin_airport_id = 8 AND day = 28;
