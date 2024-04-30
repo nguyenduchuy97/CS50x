52497df89bd4913ecd7af60222327f3d415f8411
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-04-30 02:32:23 +0000

    Tue, Apr 30, 2024, 9:32 AM +07:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -6,4 +6,21 @@ SELECT * FROM crime_scene_reports WHERE month = 7 AND day = 28;
 -- See the interview with three transcripts of witnesses.
 SELECT * FROM interviews WHERE year = 2023 AND month = 7 AND day = 28;
 
--- See 
+-- See Bekery security logs as Ruth have said.
+SELECT * FROM people WHERE license_plate =
+(SELECT license_plate FROM bakery_security_logs
+WHERE month = 7 AND day = 28 AND hour = 10 AND minute > 15
+AND minute < 25 AND activity = "exit");
+
+-- See the ATM transaction on Leggett Street
+SELECT * FROM people WHERE id =
+(SELECT person_id FROM bank_accounts WHERE account_number =
+(SELECT account_number FROM atm_transactions WHERE year = 2023
+AND month = 7 AND day = 28 AND atm_location = "Leggett Street"
+AND transaction_type = "withdraw"));
+
+-- See the phone calls logs
+SELECT * FROM people WHERE phone_number =
+(SELECT caller FROM phone_calls WHERE year = 2023
+AND month = 7 AND day = 28 AND duration < 60);
+
