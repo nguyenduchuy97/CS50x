6db5906199d6813066d252e60f5ef7da4dd9aa4e
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-04-30 03:29:08 +0000

    Tue, Apr 30, 2024, 10:29 AM +07:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -11,7 +11,7 @@ SELECT * FROM interviews WHERE year = 2023 AND month = 7 AND day = 28;
 SELECT * FROM people WHERE license_plate IN
 (SELECT license_plate FROM bakery_security_logs WHERE month = 7
 AND day = 28 AND hour = 10 AND minute > 5 AND minute < 25 AND activity = "exit")
--- Select the ATM transaction on Leggett Street
+/* Select the ATM transaction on Leggett Street*/
 AND id IN (SELECT person_id FROM bank_accounts WHERE account_number
 IN (SELECT account_number FROM atm_transactions WHERE year = 2023
 AND month = 7 AND day = 28 AND atm_location = "Leggett Street"
