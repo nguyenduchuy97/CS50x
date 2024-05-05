81a2322701a0101f05279452f9f5431970f8ce19
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-05-05 15:39:06 +0000

    Sun, May 5, 2024, 10:39 PM +07:00

--- homepage/script.js
+++ homepage/script.js
@@ -2,5 +2,5 @@ document.addEventListener('DOMContentLoaded', function() {
     setInterval(function() {
         var blinkingText = document.getElementById('blinkingText');
         blinkingText.classList.toggle('blinking');
-    }, 900); // Adjust the interval duration (milliseconds)
+    }, 400); // Adjust the interval duration (milliseconds)
 });
