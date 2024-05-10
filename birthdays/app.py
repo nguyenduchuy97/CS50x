cfe5e7a7b5a8502d8e227281ad2002266d9ae9c1
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-05-10 13:21:04 +0000

    Fri, May 10, 2024, 8:21 PM +07:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -38,8 +38,9 @@ def index():
 
         # TODO: Display the entries in the database on index.html
         names = db.execute("SELECT name FROM birthdays")
-        month_days = db.execute("SELECT month,day FROM birthdays")
+        months = db.execute("SELECT month FROM birthdays")
+        days = db.execute("SELECT day FROM birthdays")
 
-        return render_template("index.html", names=names, month_days=month_days)
+        return render_template("index.html", names=names, months=months, days=days)
 
 
