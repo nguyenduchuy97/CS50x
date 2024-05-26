d6d9ef01540f17936176b52b681db5a00881ef4a
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-05-26 03:54:41 +0000

    Sun, May 26, 2024, 10:54 AM +07:00

--- project/app.py
+++ project/app.py
@@ -1,5 +1,7 @@
 from art import text2art
 
+
+
 # Generate ASCII art text
 greet1 = text2art("Hash Generator by")
 greet2 = text2art(" Huy", font='block', chr_ignore=True)
@@ -7,3 +9,7 @@ greet2 = text2art(" Huy", font='block', chr_ignore=True)
 # Print the ASCII art
 print(greet1)
 print(greet2)
+
+
+def hash_type():
+    
