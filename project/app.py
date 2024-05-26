eb9a6ef27dd99115abbc40b4c7d44d433b19037c
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-05-26 03:45:42 +0000

    Sun, May 26, 2024, 10:45 AM +07:00

--- project/app.py
+++ project/app.py
@@ -3,7 +3,7 @@ from art import text2art
 # Generate ASCII art text
 
 art = text2art("GFG", font='block', chr_ignore=True)
-
+a = text2art("Hello")
 # Print the ASCII art
-print(ascii_art)
+print(a)
 print(art)
