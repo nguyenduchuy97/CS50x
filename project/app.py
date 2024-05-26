61d10c3b1bdc29d51ec83c5ae9c149293048c9b5
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-05-26 04:31:57 +0000

    Sun, May 26, 2024, 11:31 AM +07:00

--- project/app.py
+++ project/app.py
@@ -13,11 +13,16 @@ print(greet2)
 
 def main():
     while True:
-        print("""Choose one of the hash algorithms.
-    'SHA-1' - 'SHA-224' - 'SHA-256' - 'SHA-384' - 'SHA512'
-    'SHA3_224' - 'SHA3_256' - 'SHA3_384' - 'SHA3_512'
-    * Press """)
-        hash_type = input("Choose a hash algorithm: ")
+        print("""Choose one of the hash algorithms:
+
+                'SHA-1' - 'SHA-224' - 'SHA-256' - 'SHA-384' - 'SHA512'
+                'SHA3_224' - 'SHA3_256' - 'SHA3_384' - 'SHA3_512'
+
+    *! Press 'Ctrl + C' or 'exit()' to quit.
+              """)
+        hash_type = input("Choose a hash algorithm: ").lower()
+        if hash_type == 'exit()':
+            break
 
 if __name__ == '__main__':
     main()
