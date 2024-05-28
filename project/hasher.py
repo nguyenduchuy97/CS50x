1fabcd9d1fadd3454d29525d656dbef557bdee3a
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-05-28 11:52:56 +0000

    Tue, May 28, 2024, 6:52 PM +07:00

--- project/hasher.py
+++ project/hasher.py
@@ -1,104 +1,63 @@
-from art import text2art
 import hashlib
+from Crypto.Hash import (
+    MD2, MD4, MD5, SHA1, SHA224, SHA256, SHA384, SHA512, SHA3_224, SHA3_256,
+    SHA3_384, SHA3_512, BLAKE2b, BLAKE2s, SHAKE128, SHAKE256, HMAC, CMAC
+)
 
-# Generate ASCII art text
-greet1 = text2art("Hash Generator by")
-greet2 = text2art(" Huy", font='block', chr_ignore=True)
+# Mapping algorithm names to their respective functions.
+pycryptodome_algorithms = {
+    'MD2': MD2, 'MD4': MD4, 'MD5': MD5, 'SHA1': SHA1,
+    'SHA224': SHA224, 'SHA256': SHA256, 'SHA384': SHA384,
+    'SHA512': SHA512, 'SHA3_224': SHA3_224, 'SHA3_256': SHA3_256,
+    'SHA3_384': SHA3_384, 'SHA3_512': SHA3_512, 'BLAKE2b': BLAKE2b,
+    'BLAKE2s': BLAKE2s, 'SHAKE128': SHAKE128, 'SHAKE256': SHAKE256,
+    'HMAC': HMAC, 'CMAC': CMAC
+}
 
-# Print the ASCII art
-print(greet1)
-print(greet2)
+def hash_message(message, algorithm):
+    """
+    Hash a message using the specified algorithm.
 
-# Main function
-def main():
-    # Hashing Algorithms in use.
-    alg  = ["MD5", "SHA_1", "SHA_224", "SHA_256", "SHA_384", "SHA512", "SHA3_224", "SHA3_256", "SHA3_384", "SHA3_512", "SHAKE_128", "SHAKE_256"]
-    # Make a loop for input.
-    while True:
-        # Print description of algorithms in use.
-        print("""Choose one of the hash algorithms: MD5 , SHA_1 , SHA_224 , SHA_256 , SHA_384 , SHA512 ,
-              SHA3_224 , SHA3_256 , SHA3_384 , SHA3_512 , SHAKE_128 , SHAKE_256
-!!! Press "Ctrl + C" or "exit()" to quit.
-              """)
-        # Input a hashing algorithm type.
-        hash_type = input("Choose a hash algorithm: ").upper()
+    :param message: The message to hash (string).
+    :param algorithm: The hashing algorithm to use (string).
+    :return: The hexadecimal hash of the message.
+    """
+    # Try hashlib first
+    try:
+        hash_obj = hashlib.new(algorithm)
+        hash_obj.update(message.encode('utf-8'))
+        return hash_obj.hexdigest()
+    except ValueError:
+        pass
 
-        # Set a command for exit out of program.
-        if hash_type == 'exit()':
-            break
-        # Look into algorithm list for which user want to use.
-        if hash_type in alg:
-            text = input("Input your password: ")
-
-            # Limit the length of the password.
-            if len(text) > 50 or len(text) < 1:
-                print("Your password length between 1 to 50 characters")
-                continue
-            # Add an additional variable for SHAKE-type algorithms.
-            if hash_type == "SHAKE_128" or hash_type == "SHAKE_256":
-                try:
-                    length = int(input("Input length hash: "))
-                    # Limit the length of the algorithm output.
-                    if length < 1 or length > 99 or not length:
-                        print("\nThe length must be between 1 - 255 chars\n")
-                        continue
-
-                except ValueError:
-                    print("Invalid length. Please enter a number between 1 and 255 of hash length.")
-                    continue
-                # Output of SHAKE-types
-                hashee = hash_generator(hash_type, text, length)
-                print(f"{hash_type} hash: {hashee}\n Length: {length}")
-                break
+    # If hashlib fails, try pycryptodome
+    try:
+        if algorithm in pycryptodome_algorithms:
+            if algorithm.startswith('SHAKE'):
+                hash_obj = pycryptodome_algorithms[algorithm].new(data=message.encode('utf-8'), length=64)
             else:
-                # Output of SHA-types
-                hashee = hash_generator(hash_type, text)
-                print(f"{hash_type} hash: {hashee}")
+                hash_obj = pycryptodome_algorithms[algorithm].new(data=message.encode('utf-8'))
+            return hash_obj.hexdigest()
         else:
-            print("Invalid hash type.")
-        break
+            return f"Algorithm {algorithm} is not supported."
+    except Exception as e:
+        return str(e)
 
-# Hash generator function
-def hash_generator(hash_alg, text, length=None):
+def main():
+    # Get the list of available algorithms from both libraries
+    hashlib_algorithms = hashlib.algorithms_available
+    pycryptodome_algorithms_list = list(pycryptodome_algorithms.keys())
+    all_algorithms = sorted(set(hashlib_algorithms).union(pycryptodome_algorithms_list))
 
-    match hash_alg:
+    print("Available algorithms:", ", ".join(all_algorithms))
 
-        case 'MD5':
-            return hashlib.md5(text.encode()).hexdigest()
+    # Get user input for the message and algorithm
+    message = input("Enter the message to hash: ")
+    algorithm = input("Enter the hashing algorithm to use: ")
 
-        case 'SHA_1':
-            return hashlib.sha1(text.encode()).hexdigest()
+    # Hash the message using the specified algorithm
+    hashed_message = hash_message(message, algorithm)
+    print(f"Hashed message using {algorithm}: {hashed_message}")
 
-        case 'SHA_224':
-            return hashlib.sha224(text.encode()).hexdigest()
-
-        case 'SHA_256':
-            return hashlib.sha256(text.encode()).hexdigest()
-
-        case 'SHA_384':
-            return hashlib.sha384(text.encode()).hexdigest()
-
-        case 'SHA512':
-            return hashlib.sha512(text.encode()).hexdigest()
-
-        case 'SHA3_224':
-            return hashlib.sha3_224(text.encode()).hexdigest()
-
-        case 'SHA3_256':
-            return hashlib.sha3_256(text.encode()).hexdigest()
-
-        case 'SHA3_384':
-            return hashlib.sha3_384(text.encode()).hexdigest()
-
-        case 'SHA3_512':
-            return hashlib.sha3_512(text.encode()).hexdigest()
-
-        case 'SHAKE_128':
-            return hashlib.shake_128(text.encode()).hexdigest(length)
-
-        case 'SHAKE_256':
-            return hashlib.shake_256(text.encode()).hexdigest(length)
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
