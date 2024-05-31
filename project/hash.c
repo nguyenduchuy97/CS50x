443c1d10d6b40ea8eb37aaa3af3d35aa46bfff69
nguyenduchuy97 <161210227+nguyenduchuy97@users.noreply.github.com>
2024-05-31 09:18:16 +0000

    Fri, May 31, 2024, 4:18 PM +07:00

--- project/hash.c
+++ project/hash.c
@@ -3,47 +3,67 @@
 #include <string.h>
 #include <openssl/evp.h>
 
+// Set buffer size for reading an input file.
+#define BUFFER_SIZE 4096
+
+// Print output of a hashed result.
 void print_hash(const char *algorithm, const unsigned char *digest, unsigned int length) {
     printf("%s: ", algorithm);
     for (unsigned int i = 0; i < length; i++) {
+        // The %02x is the output for 
         printf("%02x", digest[i]);
     }
     printf("\n");
 }
 
-void hash_input(const char *input, const char *algorithm) {
+void hash_file(const char *filename, const char *algorithm) {
     EVP_MD_CTX *mdctx;
     const EVP_MD *md;
     unsigned char digest[EVP_MAX_MD_SIZE];
     unsigned int digest_len;
+    unsigned char buffer[BUFFER_SIZE];
+    size_t bytes_read;
+    FILE *file;
+
+    file = fopen(filename, "rb");
+    if (!file) {
+        perror("Error opening file");
+        exit(1);
+    }
 
     md = EVP_get_digestbyname(algorithm);
     if (!md) {
         printf("Unknown message digest %s\n", algorithm);
+        fclose(file);
         exit(1);
     }
 
     mdctx = EVP_MD_CTX_new();
     EVP_DigestInit_ex(mdctx, md, NULL);
-    EVP_DigestUpdate(mdctx, input, strlen(input));
+
+    while ((bytes_read = fread(buffer, 1, BUFFER_SIZE, file)) != 0) {
+        EVP_DigestUpdate(mdctx, buffer, bytes_read);
+    }
+
     EVP_DigestFinal_ex(mdctx, digest, &digest_len);
     EVP_MD_CTX_free(mdctx);
+    fclose(file);
 
     print_hash(algorithm, digest, digest_len);
 }
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
-        printf("Usage: %s <algorithm> <input>\n", argv[0]);
+        printf("Usage: %s <algorithm> <filename>\n", argv[0]);
         return 1;
     }
 
     const char *algorithm = argv[1];
-    const char *input = argv[2];
+    const char *filename = argv[2];
 
     OpenSSL_add_all_digests();
 
-    hash_input(input, algorithm);
+    hash_file(filename, algorithm);
 
     EVP_cleanup();
     return 0;
